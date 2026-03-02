0ea41faeb8ded58128bf3f9396162430162bceb3
Emre Ekinci <emree3657@gmail.com>
2026-03-02 16:20:03 +0300

    refactor: simplify login schema and add trim both

--- src/schemas/auth-schema.ts
+++ src/schemas/auth-schema.ts
@@ -6,8 +6,8 @@ export type LoginInput = z.infer<typeof loginSchema>;
 // Create register schema with Zod
 export const registerSchema = z
   .object({
-  username: z.string().min(5, "Username must be at least 5 characters long"),
-  email: z.email("Please enter a valid emaild adrress"),
+  username: z.string().trim().min(5, "Username must be at least 5 characters long").max(30, "Username must be at most 30 characters long"),
+  email: z.email("Please enter a valid email address"),
   password: z
     .string()
     .min(8, "Password must be at least 8 characters long")
@@ -23,11 +23,9 @@ export const registerSchema = z
 
 // Create login schema with Zod
 export const loginSchema = z.object({
-username: z.string().min(5, "Username must be at least 5 characters long"),
+username: z.string().trim().min(1, "Username is required"),
 password: z
   .string()
-  .min(8, "Password must be at least 8 characters long")
-  .regex(/[A-Za-z]/, "Password must contain at least one letter")
-  .regex(/\d/, "Password must contain at least one number"),  
+  .min(1, "Password is required") 
 });
 
