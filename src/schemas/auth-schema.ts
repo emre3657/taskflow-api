0812b7c5822c0b68fceeeff447f2fcb7c4e0fb9b
Emre Ekinci <emree3657@gmail.com>
2026-03-25 18:00:19 +0300

    feat(auth): limit password with max value 20

--- src/schemas/auth-schema.ts
+++ src/schemas/auth-schema.ts
@@ -11,9 +11,10 @@ export const registerSchema = z
   password: z
     .string()
     .min(8, "Password must be at least 8 characters long")
+    .max(20, "Password must be at most 20 characters long")
     .regex(/[A-Za-z]/, "Password must contain at least one letter")
     .regex(/\d/, "Password must contain at least one number"),
-  repassword: z.string().min(8)
+  repassword: z.string().min(8).max(20)
   })
   .refine((data) => data.password === data.repassword, {
     error: "Passwords do not match", 
