e5bc1977ef7fd43545c3393bec600a90b202aa8a
Emre Ekinci <emree3657@gmail.com>
2026-04-14 23:36:45 +0300

    feat(error): include conflicting fields in conflict errors

--- src/errors/conflict-error.ts
+++ src/errors/conflict-error.ts
@@ -1,8 +1,16 @@
 import { StatusCodes } from "http-status-codes";
 import { CustomAPIError } from "./custom-api-error.js";
 
+export type ConflictErrorOptions = {
+  field: string;
+  message: string;
+};
+
 export class ConflictError extends CustomAPIError {
-  constructor(message: string = "Conflict") {
+  errors: Array<ConflictErrorOptions>;
+
+  constructor(message: string = "A conflict occurred", errors: Array<ConflictErrorOptions>) {
     super(message, StatusCodes.CONFLICT, "CONFLICT");
+    this.errors = errors;
   }
 } 
--- src/errors/custom-api-error.ts
+++ src/errors/custom-api-error.ts
@@ -1,11 +1,15 @@
+import type { ConflictErrorOptions } from "./conflict-error.js";
+
 export abstract class CustomAPIError extends Error {
   public readonly statusCode: number;
   public readonly code: string;
+  public readonly errors: Array<ConflictErrorOptions>;
 
-  constructor(message: string, statusCode: number, code: string) {
+  constructor(message: string, statusCode: number, code: string, errors: Array<ConflictErrorOptions> = []) {
     super(message);
     this.statusCode = statusCode;
     this.code = code;
+    this.errors = errors; // Just using for conflict error.
 
     Error.captureStackTrace(this, this.constructor);
   }
